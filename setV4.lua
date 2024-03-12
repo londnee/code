8cd30ee1dcec44bc8b1713a54b40415336b32c6e
londnee <140787703+londnee@users.noreply.github.com>
2024-03-12 10:36:33 +0700

    Create setV4.lua

---
+++ setV4.lua
@@ -0,0 +1,20 @@
+local Setting = {
+    ['Sever Vip Faster Evo V4'] = false, -- ถ้าไม่ได้ฟามในเซิฟ Vip ให้ปรับ false / If you don't get it in the Vip server, set it to false.
+    ['Auto Evo Race V4'] = true,
+    Race_V4 = { 
+        ['Helper'] = 'SandBoxGX_6D621', 
+        ['Helper2'] = 'Sailom_142', 
+        ['Farm'] = { 
+            'bgfdsa79875',
+            'mnbvcxz90424',
+            'bgfdsa67715'
+        }
+    },
+    -- Booster CPU
+    ['Close Ui'] = false,
+    ['White Screen'] = true, 
+    ['FPS Booster'] = true,
+    ['Destroy Ui'] = true -- ไม่ใช้ Ui 
+}
+print('8989')
+return Setting
